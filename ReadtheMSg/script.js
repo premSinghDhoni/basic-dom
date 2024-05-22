fb7dbe42a77d03527228977dc22904d1fceed7d7
premSinghDhoni <premsinghdhoni35@gmail.com>
2024-05-22 17:21:03 +0530

    2 more mini dom function

---
+++ ReadtheMSg/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".formText");
+const msg = document.querySelector("#msg");
+form.addEventListener("submit", (e) => {
+  let message = "";
+  e.preventDefault();
+  message = msg.value;
+  console.log(message);
+  let p = e.target.parentNode.children[2];
+p.textContent+=` ${ message}`;
+msg.value=''
+});
